66bcedcdcd8fa8d71ef2a471eaeb7ba44257b610
Nguyen Dang Duc <dev.ndangduc.bn@gmail.com>
2021-07-22 08:56:39 +0700

    Update Code

---
+++ 81-array-declaration/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+/*
+	- Ngày: 22.07.2021
+	- By: Nguyễn Đăng Đức
+ */
+func main()  {
+	// 01. Khai báo mảng
+	var (
+		myArray1  [8]int
+		myArray2  [8]string
+		myArray3  [8]complex128
+	)
+
+
+	// 02. In ra
+	fmt.Println(myArray1)
+	fmt.Println(myArray2)
+	fmt.Println(myArray3)
+
+}
